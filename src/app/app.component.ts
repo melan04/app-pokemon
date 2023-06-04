89279fd6e23fb5f966e7df2ed978c2b8450f48df
Mélany Condé <melany.conde@gmail.com>
2023-06-04 22:48:13 +0200

    add template pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { POKEMONS } from "./mock-pokemon-list";
+import { Pokemon } from "./pokemon";
 
 @Component({
-  selector: 'app-root',
-  template:  `<h1>Welcome to {{title}}! </h1>`
+  selector: "app-root",
+  templateUrl: "app.component.html",
 })
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
 
-export class AppComponent {
-  title = 'ng-pokemon-app';
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon|undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == +pokemonId
+    );
+    
+    if (pokemon) {
+      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokémon qui n'existe pas.`);
+      this.pokemonSelected = pokemon;
+    }
+  }
 }
